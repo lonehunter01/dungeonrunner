273dc3af075b6b73718c5c2cd9caef8756fa0d1f
Scott Holt <sholt1234@gmail.com>
2018-08-12 00:26:56 -0400

    Updated namespace, changed program message

--- DungeonRunner.Console/Program.cs
+++ DungeonRunner.Console/Program.cs
@@ -1,12 +1,12 @@
 ﻿using System;
 
-namespace DungeonRunner.Console
+namespace DungeonRunner
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Testing");
         }
     }
 }
